fbc9603450fbf1bb2b52f87973f446152d6f1202
Gustavo <gustofa@gmail.com>
2021-10-27 01:56:00 -0300

    Product and Rating components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,62 +1,26 @@
 //import logo from './logo.svg';
 import "./App.css";
 import data from "./data";
-
+import Product from "./components/Product";
 function App() {
   return (
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand" href="index.html">
+          <a className="brand" href="/">
             amazon-clone
           </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div>
           <div className="row center">
             {data.products.map((product) => (
-              <div key={product._id} className="card">
-                <a href={`/product/${product._id}`}>
-                  <img
-                    className="medium"
-                    src={product.image}
-                    alt={product.name}
-                  />
-                </a>
-                <div className="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div className="rating">
-                    <span>
-                      {" "}
-                      <i className="fa fa-star"></i>{" "}
-                    </span>
-                    <span>
-                      {" "}
-                      <i className="fa fa-star"></i>{" "}
-                    </span>
-                    <span>
-                      {" "}
-                      <i className="fa fa-star"></i>{" "}
-                    </span>
-                    <span>
-                      {" "}
-                      <i className="fa fa-star"></i>{" "}
-                    </span>
-                    <span>
-                      {" "}
-                      <i className="fa fa-star"></i>{" "}
-                    </span>
-                  </div>
-                  <div className="price">{product.price}</div>
-                </div>
-              </div>
+              <Product key={product._id} product={product} />
             ))}
           </div>
         </div>
