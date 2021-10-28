d9601ba205614d0cfb3e7aff9b8c129ad0c36165
Gustavo <gustofa@gmail.com>
2021-10-27 22:56:46 -0300

    Implement React Router and. Add Home page and Product page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,31 @@
-//import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 import "./App.css";
-import data from "./data";
-import Product from "./components/Product";
+
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            amazon-clone
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product._id} product={product} />
-            ))}
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazon-clone
+            </a>
           </div>
-        </div>
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/" component={HomePage} exact />
+          <Route path="/product/:id" component={ProductPage} />
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
