eaa05b44b2824ddd10e3ce38aa8a7eac6c6fd5b3
Gustavo <gustofa@gmail.com>
2021-11-08 02:42:39 -0300

    Sign in and Sign out functionalities

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,21 @@ import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import "./App.css";
 import CartPage from "./pages/cartPage";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import SigninPage from "./pages/SigninPage";
+import { signout } from "./actions/userActions";
 
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const dispatch = useDispatch();
+  const signoutHandler = () => {
+    dispatch(signout());
+  };
+
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -25,12 +35,28 @@ function App() {
                 <span className="badge">{cartItems.length}</span>
               )}
             </Link>
-            <Link to="/signin">Sign In</Link>
+
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name}
+                  <i className="fa fa-caret-down"></i>{" "}
+                </Link>
+                <ul className="dropdown-content">
+                  <Link to="#signout" onClick={signoutHandler}>
+                    Sign Out
+                  </Link>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
           </div>
         </header>
         <main>
           <Route path="/cart/:id?" component={CartPage}></Route>
           <Route path="/" component={HomePage} exact />
+          <Route path="/signin" component={SigninPage} />
           <Route path="/product/:id" component={ProductPage} />
         </main>
         <footer className="row center">All right reserved</footer>
